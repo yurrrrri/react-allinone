6344795773488f259023e9119f51710614af7cf5
yurison <yu_ri_son@naver.com>
2024-03-13 16:18:46 +0900

    change player

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,11 +5,15 @@ import "./Board.css";
 
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isX, setIsX] = useState(true);
+
+  const status = `Next player: ${isX ? "X" : "O"}`;
 
   function handleClick(i) {
     const newSquares = squares.slice();
-    newSquares[i] = "X";
+    newSquares[i] = isX ? "X" : "O";
     setSquares(newSquares);
+    setIsX((current) => !current);
   }
 
   function renderSquare(i) {
@@ -18,7 +22,7 @@ const Board = () => {
 
   return (
     <div>
-      <div className="status">"Next player: X"</div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
