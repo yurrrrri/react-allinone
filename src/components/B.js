a21d735929467cbae7ed494afc45ec000ac37e4f
yurison <yu_ri_son@naver.com>
2024-03-17 22:51:07 +0900

    B component

--- src/components/B.js
+++ src/components/B.js
@@ -1,9 +1,31 @@
+function Message({ message }) {
+  return <p>{message}</p>;
+}
+
+function ListItem({ post }) {
+  return (
+    <li key={post.id}>
+      <p>{post.title}</p>
+    </li>
+  );
+}
+
+function List({ posts }) {
+  return (
+    <ul>
+      {posts.map((post) => (
+        <ListItem key={post.id} post={post} />
+      ))}
+    </ul>
+  );
+}
+
 export default function B({ message, posts }) {
   return (
     <div>
       <h1>B Component</h1>
-      <p>{message}</p>
-      <ul>{posts}</ul>
+      <Message message={message} />
+      <List posts={posts} />
     </div>
   );
 }
