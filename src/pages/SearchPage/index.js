891ec72f7021e03812343740b5f2fd60dd4add7e
yurison <yu_ri_son@naver.com>
2024-03-24 21:56:04 +0900

    search page(1)

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,4 +1,34 @@
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "../../api/axios";
+
 const SearchPage = () => {
+  const [searchResults, setSearchResults] = useState([]);
+
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+
+  let query = useQuery();
+  const searchTerm = query.get("q");
+
+  useEffect(() => {
+    if (searchTerm) {
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm]);
+
+  const fetchSearchMovie = async (searchTerm) => {
+    try {
+      const response = await axios.get(
+        `/search/multi?include_adult=false&query=${searchTerm}`
+      );
+      setSearchResults(response.data.result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return <div></div>;
 };
 
