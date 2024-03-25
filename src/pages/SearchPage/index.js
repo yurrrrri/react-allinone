a8fd115da8e885b39c93dd31022b9b6c492ad335
yurison <yu_ri_son@naver.com>
2024-03-25 23:18:09 +0900

    search page done

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import axios from "../../api/axios";
 
 const SearchPage = () => {
   const [searchResults, setSearchResults] = useState([]);
+  const navigate = useNavigate();
 
   const useQuery = () => {
     return new URLSearchParams(useLocation().search);
@@ -23,13 +24,50 @@ const SearchPage = () => {
       const response = await axios.get(
         `/search/multi?include_adult=false&query=${searchTerm}`
       );
-      setSearchResults(response.data.result);
+      setSearchResults(response.data.results);
     } catch (error) {
       console.log(error);
     }
   };
 
-  return <div></div>;
+  if (searchResults.length > 0) {
+    return (
+      <section className="search-container">
+        {searchResults.map((movie) => {
+          if (
+            (movie.backdrop_path !== null) &
+            (movie.media_type !== "person")
+          ) {
+            const movieImageUrl =
+              "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+
+            return (
+              <div className="movie" key={movie.id}>
+                <div
+                  className="movie__column-poster"
+                  onClick={() => navigate(`/${movie.id}`)}
+                >
+                  <img
+                    src={movieImageUrl}
+                    alt="movie"
+                    className="movie__poster"
+                  />
+                </div>
+              </div>
+            );
+          }
+        })}
+      </section>
+    );
+  } else {
+    return (
+      <section className="no-results">
+        <div className="no-results__text">
+          <p>찾고자 하는 검색어 "${searchTerm}" 에 맞는 영화가 없습니다.</p>
+        </div>
+      </section>
+    );
+  }
 };
 
 export default SearchPage;
