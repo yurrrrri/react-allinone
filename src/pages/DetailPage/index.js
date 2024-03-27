1972e82c567de454e6af9439f198df342d47206e
yurison <yu_ri_son@naver.com>
2024-03-27 23:11:18 +0900

    detail page

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,5 +1,32 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import useParams from "react-router-dom";
+import axios from "../../api/axios";
+
 const DetailPage = () => {
-  return <div></div>;
+  let { movieId } = useParams();
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(`/movie/${movieId}`);
+      setMovie(response.data);
+    }
+
+    fetchData();
+  }, [movieId]);
+
+  if (!movie) return null;
+
+  return (
+    <section>
+      <img
+        className="modal__poster-img"
+        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+        alt="movie"
+      />
+    </section>
+  );
 };
 
 export default DetailPage;
